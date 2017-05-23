48e3628a1d4069642b8c0cd40582f7300a16d822
Kes siis mu kui Matu <Kes siis mu kui Matu@DESKTOP-CSIQTVF>
2017-05-23 17:53:01 +0300

    Add project files.

---
+++ LocalizeThis/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace LocalizeThis
+{
+    [Activity(Label = "LocalizeThis", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar",
+        ConfigurationChanges = ConfigChanges.Locale | ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var lang = Resources.Configuration.Locale; // eg. "es_ES"
+
+
+
+
+
+            SetContentView (Resource.Layout.Main);
+            var button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate {
+                var Text = Resources.GetText(Resource.String.Localization);
+                var asi = FindViewById<TextView>(Resource.Id.textView1);
+                asi.Text = Text;
+            };
+
+        }
+    }
+}
+
